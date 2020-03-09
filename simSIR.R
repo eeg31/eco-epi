c1b481b094bb6f0cafaf9443c2e1cdc7b5f45b45
Emma Glennon <eeglennon@gmail.com>
2020-03-09 21:51:22 +0000

    added vaccination
    must re-install generalSEIR from source for this to work

--- simSIR.R
+++ simSIR.R
@@ -1,42 +1,40 @@
 require(generalSEIR)
 
 simSIR <- function(tmax,
-                   var,
+                   var=NULL,
                    par,
                    inc=12) {
-  nevents <- 11
   
   .b <- as.numeric(par['b'])
   .m <- as.numeric(par['m'])
   .R0 <- as.numeric(par['R0'])
   .gamma <- as.numeric(par['gamma'])
   .N <- as.numeric(par['N'])
+  .v <- 0
+  if(!is.null(par['v'])) .v <- par['v']
   
   epsilon <- 0
   omega <- 0
   gamma <- par$gamma
   rho <- 0
   sigma <- 0
-  whichBeta <- 2
-  whichGamma <- 2
-  whichSigma <- 1
-  beta <- generalSEIR::getBeta(.R0, rho, epsilon, sigma, .m, .gamma, .N, whichBeta)
   
+  eq <- TRUE
+  S0 <- NULL
+  I0 <- NULL
+  E0 <- NULL
+  if(!is.null(var)){
+    S0 <- var[1]
+    E0 <- 0
+    I0 <- var[2]
+    
+    eq <- FALSE
+  }
+  sim <- simSEIRV(N=.N, S0=S0, E0=E0, I0=I0, model=41,
+                  R0=.R0, gamma=gamma, sigma=sigma, rho=rho, 
+                  epsilon=epsilon, omega=omega, v=.v,
+                  b=.b, m=.m,
+                  tmax=tmax, inc=inc, equilibrium=eq)
   
-  par <- c(beta, .gamma, sigma, rho, epsilon, omega, .b, .m)
-  spec <- as.integer(c(whichBeta, whichSigma, whichGamma))
-  
-  #simulate nsim times and plot together
-  init <- as.integer(c(var[1], 0, var[2], var[3], 0))
-  sim <- generalSEIR::simulate1(init, par, spec, nevents, tmax, inc)
-  S <- sim[1:(tmax*inc)]
-  I <- sim[(2*tmax*inc+1):(3*tmax*inc)]
-  R <- sim[(3*tmax*inc+1):(4*tmax*inc)]
-  incidence <- sim[(4*tmax*inc+1):(5*tmax*inc)]
-  
-  N <- S + I + R
-  N[N==0] <- .N
-  extinct <- I==0
-  
-  return(list(incidence=incidence, extinct=extinct, N = N))
+  return(sim)
 }
